b4eef23e1e818c39d5e474e8337761481dbd2e7d
4maziar <53979524+8maziar@users.noreply.github.com>
2023-05-24 17:08:19 +0100

    complete dna repairs

--- dna-pairs/__tests__/dna-pairs.test.js
+++ dna-pairs/__tests__/dna-pairs.test.js
@@ -1,7 +1,24 @@
-const dnaPairs = require('../dna-pairs.js');
+const dnaPairs = require("../dna-pairs.js");
 
-describe('dnaPairs()', () => {
-  test('ADD YOUR TEST DESCRIPTION HERE', () => {
-    // add your test assertion(s) here
+describe("takes a string of DNA and matches each base with its pair, returning a nested array", () => {
+  test("return an empty str given an empty str", () => {
+    expect(dnaPairs("")).toBe("");
+  });
+  test("return [ ['G', 'C'] ] str given 'G'", () => {
+    expect(dnaPairs("G")).toEqual([["G", "C"]]);
+  });
+  test("return [ ['A', 'T'], ['G', 'C'] ] str given 'AG'", () => {
+    expect(dnaPairs("AG")).toEqual([
+      ["A", "T"],
+      ["G", "C"],
+    ]);
+  });
+  test("return [ ['A', 'T'], ['T', 'A'], ['A', 'T'], ['G', 'C'] ] str given 'ATAG'", () => {
+    expect(dnaPairs("ATAG")).toEqual([
+      ["A", "T"],
+      ["T", "A"],
+      ["A", "T"],
+      ["G", "C"],
+    ]);
   });
 });
--- dna-pairs/dna-pairs.js
+++ dna-pairs/dna-pairs.js
@@ -1,6 +1,9 @@
 // Please do not change the name of this function
 function dnaPairs(dna) {
-  // Write your code here
+  if (!dna) return "";
+  return [...dna].map((char) => {
+    return char === "G" ? ["G", "C"] : char === "A" ? ["A", "T"] : char === "T" ? ["T", "A"] : ["C", "G"];
+  });
 }
 
 module.exports = dnaPairs;
